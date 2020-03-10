aef072972504fe4c2a9fab60f8e0f931e3ff64e6
Hiro <sham1996666@gmail.com>
2020-03-10 16:18:35 +0300

    add: instagram button

--- gulpfile.js
+++ gulpfile.js
@@ -13,11 +13,12 @@ function bs() {
   });
   watch("./*.html").on('change', browserSync.reload);
   watch("./sass/**/*.sass", serveSass);
+  watch("./sass/**/*.sсss", serveSass);
   watch("./js/*.js").on('change', browserSync.reload);
 };
 
 function serveSass() {
-  return src("./sass/**/*.sass",)
+  return src("./sass/**/*.sass", "./sass/**/*.scss")
       .pipe(sass())
       .pipe(autoprefixer({
         cascade: false
