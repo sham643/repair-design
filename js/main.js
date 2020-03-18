b6de8644c54c6e628087a6ab49f2d0c7c51d5528
Hiro <sham1996666@gmail.com>
2020-03-18 20:31:30 +0300

    refact: changed language in main.js from es6 to jquery

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-document.addEventListener("DOMContentLoaded", function(event){
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close')
-  const switchModal = () =>  {
-    modal.classList.toggle('modal--visible');
-  }
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
+
+$(document).ready(function() {
+  var modal = $('.modal'),
+    modalBtn = $('[data-toggle=modal]'),
+    closeBtn = $('.modal__close');
+
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible')
   });
-
-  closeBtn.addEventListener('click', switchModal);
-
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible')
+  })
 });
